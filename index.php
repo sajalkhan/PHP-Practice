e9c0ccc33e344a4ecb5e9207f0be6fccd11a54f9
sajalkhan <sajalhossen@yahoo.com>
2021-04-21 21:48:12 +0600

    learn php functions

--- index.php
+++ index.php
@@ -11,7 +11,53 @@
 <body>
     <!------------- Documentation -------------->
     <!-- ---- https://www.php.net/manual/en/ ---->
-    <h1>Learning php language</h1>
+    <h1>Learning php functions</h1>
+
+    <?php
+
+    //! create a simple function 
+
+    function userName($name)
+    {
+        echo "Hello  $name" . '<br/>';
+    }
+
+    userName('sajal');
+
+    //! create sum of two functions
+    function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    echo add(1, 2) . '<br/>';
+
+
+    //! create a function to sum all number using ...$num spread operator
+
+    function sum(...$num)
+    {
+        $result = 0;
+        //! way 1
+        // for ($i = 0; $i <= count($num); $i++) {
+        //     $result += $i;
+        // }
+
+        //!way 2
+        foreach ($num as $n) {
+            $result += $n;
+        }
+
+        //!way 3
+        // return array_reduce($num, fn ($carry, $n) => $carry + $n); // support php version above 7,0
+
+
+        echo $result;
+    }
+
+    sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    ?>
 </body>
 
 </html>
